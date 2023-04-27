94b17e4160990bdc67d2be137b14fafaf8193dbf
SrtHero278 <93018286+SrtHero278@users.noreply.github.com>
2023-04-26 20:52:25 -0700

    fixed edit.hx

--- Codename Char Offset Editor/charts/edit.hx
+++ Codename Char Offset Editor/charts/edit.hx
@@ -1,10 +1,8 @@
-import funkin.system.MusicBeatSubstate;
-
 function update() {
 	if (FlxG.keys.justPressed.SIX) {
 		paused = true;
 		persistentUpdate = false;
 		persistentDraw = true;
-		openSubState(new MusicBeatSubstate(true, "SelectChar"));
+		openSubState(new ModSubState("SelectChar"));
 	}
 }
