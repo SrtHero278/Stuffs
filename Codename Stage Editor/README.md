434f2adb0ec8d939e526e1569c29cbcc2788fa3c
SrtHero278 <93018286+SrtHero278@users.noreply.github.com>
2023-10-03 07:43:34 -0700

    no wya stage editor? (WIP

---
+++ Codename Stage Editor/README.md
@@ -0,0 +1,3 @@
+# DONT PUT THIS IN A MOD FOLDER.
+
+put this folder in **GAMEFOLDER/addons**! (You may need to make the addons folder.)
---
+++ Codename Stage Editor/data/states/EditorPicker.hx
@@ -0,0 +1,25 @@
+import funkin.editors.ui.UIState;
+import flixel.effects.FlxFlicker;
+
+var stageIndex = -1;
+
+function create() {
+	for (i in 0...options.length)
+		stageIndex = (options[i].name == "Stage Editor") ? i : stageIndex;
+	if (stageIndex > -1)
+		options[stageIndex].state = ModState;
+}
+
+var overrodeFlicker = false;
+function update() {
+	if (stageIndex <= -1 || overrodeFlicker) return;
+
+	if (curSelected == stageIndex && selected && FlxFlicker.isFlickering(sprites[stageIndex].label)) {
+		FlxFlicker._boundObjects[sprites[stageIndex].label].completionCallback = function(flick) {
+			subCam.fade(0xFF000000, 0.25, false, function() {
+				FlxG.switchState(new UIState(true, "stageEditor/StageEdit"));
+			});
+		}
+		overrodeFlicker = true;
+	}
+}
